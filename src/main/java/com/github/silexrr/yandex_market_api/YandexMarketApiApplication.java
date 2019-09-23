182de0fa20d7f2190f2fc9bae246aff2f69d0c93
Anton <anton.b@jrvs.ru>
2019-09-23 15:53:55 +0300

    initial

---
+++ src/main/java/com/github/silexrr/yandex_market_api/YandexMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.silexrr.yandex_market_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YandexMarketApiApplication.class, args);
+	}
+
+}
